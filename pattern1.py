1f89b90eaf0b71d4d48f6ed04790fe7617c818b4
JAI-2002 <72140256+JAI-2002@users.noreply.github.com>
2020-10-01 00:10:40 +0530

    Better code

--- pattern1.py
+++ pattern1.py
@@ -5,9 +5,16 @@
 # # # #
 # # # # #
 
-for i in range(4):
-    for j in range(4):
-        if j>=0 and j<=i:
-            print("# ",end="")
-    print()
+# for i in range(4):
+#     for j in range(4):
+#         if j>=0 and j<=i:
+#             print("# ",end="")
+#     print()
 
+
+#this one is much better and flexible as it takes input from the user and acts acordingly
+n=int(input())
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print("#", end = "")
+    print()
